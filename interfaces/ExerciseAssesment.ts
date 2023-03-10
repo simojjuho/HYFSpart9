f114e0d9eb8c1ce885fdb8a42218f3e44047ec40
Juho Simojoki <juho.simojoki@outlook.com>
2023-03-10 13:07:26 +0200

    Exercise calculator now accepts command line arguments, passes them to parseArguments and finally returns the object.

---
+++ interfaces/ExerciseAssesment.ts
@@ -0,0 +1,9 @@
+export interface ExerciseAssesment {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: String;
+  target: number;
+  average: number;
+}
---
+++ utilities/parseArguments.ts
@@ -0,0 +1,15 @@
+export const parseArguments = (args: String[]): number[] => {
+  const amountOfArgs = args.length - 2;
+
+  if (args.length < 4) throw new Error("Not enough arguments!");
+  if (args.length > 2 + amountOfArgs) throw new Error("Too many arguments!");
+
+  let checkedArgs: number[] = [];
+  for (let i: number = 2; i < 2 + amountOfArgs; i++) {
+    if (!isNaN(Number(args[i]))) {
+      checkedArgs = [...checkedArgs, Number(args[i])];
+    } else throw new Error("Provided values were not numbers");
+  }
+
+  return checkedArgs;
+};
